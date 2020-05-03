ff4456839fc807e6ffb954481645821c95942d10
Higo Ribeiro <HORBJN@gmail.com>
2020-05-03 19:49:28 -0300

    chore: add functionality to the app

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,26 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    uploadedFiles.forEach(file => data.append('file', file.file));
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+      history.push('/');
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    setUploadedFiles(
+      files.map(file => ({
+        name: file.name,
+        file,
+        readableSize: filesize(file.size),
+      })),
+    );
   }
 
   return (
--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,10 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+const intlMonetary = Intl.NumberFormat('pt-BR', {
+  style: 'currency',
+  currency: 'BRL',
+  minimumIntegerDigits: 2,
+});
+
+const formatValue = (value: string | number): string =>
+  intlMonetary.format(Number(value));
 
 export default formatValue;
